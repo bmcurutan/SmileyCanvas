3d69b982f3e63efbdb1d1cab3cf0bb7cdb2f20d1
Bianca Curutan <bmcurutan@gmail.com>
2016-11-03 19:23:33 -0700

    milestone2

--- SmileyCanvas/ViewController.swift
+++ SmileyCanvas/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var trayOriginalCenter: CGPoint!
 
+    @IBOutlet weak var trayView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTrayPanGesture(_ sender: UIPanGestureRecognizer) {
+        // Absolute (x,y) coordinates in parent view (parentView should be
+        // the parent view of the tray)
+        let point = sender.location(in: trayView)
+        
+        if sender.state == .began {
+            print("Gesture began at: \(point)")
+            trayOriginalCenter = self.trayView.center
+        } else if sender.state == .changed {
+            print("Gesture changed at: \(point)")
+            trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + sender.translation(in: trayView).y)
+            
+        } else if sender.state == .ended {
+            print("Gesture ended at: \(point)")
+        }
+    }
 
 }
 
